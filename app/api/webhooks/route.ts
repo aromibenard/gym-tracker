b51d32b1ae5d688b22c8a80d7cbee0d96b82d7af
aromibenard <aromibenard@gmail.com>
2026-05-11 12:26:22 +0300

    Implement webhook processing for user creation and handle missing secret

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,6 +1,45 @@
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { Webhook } from "svix"
+import { db } from "@/client";
+
+
 export async function POST(request: Request) {
     try {
         const secret = process.env.CLERK_WEBHOOK_SECRET_KEY;
+        if (!secret) return new Response("Missing secret", { status: 500 })
+
+        const wh = new Webhook(secret)
+        const body = await request.text()
+        const headerPayload = await headers()
+
+        const event = wh.verify(body, {
+            "svix-id": headerPayload.get("svix-id")!,
+            "svix-timestamp": headerPayload.get("svix-timestamp")!,
+            "svix-signature": headerPayload.get("svix-signature")!,
+        }) as WebhookEvent
+
+        if (event.type === "user.created") {
+            const { id, email_addresses, first_name, last_name } = event.data
+            
+            try {
+                await db.user.upsert({
+                    where: { clerkId: id },
+                    update: {},
+                    create: {
+                        clerkId: id,
+                        email: email_addresses[0]?.email_address || "",
+                        name: `${first_name || ""} ${last_name || ""}`.trim(),
+                    }
+                })
+            } catch (dbError) {
+                console.error('Database error:', dbError)
+                return new Response('Create User failed', { status: 500 })
+            }
+
+        }
+
+        return new Response("Webhook processed", { status: 200 });
     } catch (error) {
         console.error("Error verifying webhook:", error);
         return new Response("Error verifying webhook", { status: 400 });
